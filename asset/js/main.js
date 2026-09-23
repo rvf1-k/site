6bb3675bd95a3f30bb1e10918f8f0607024bd12a
Dansoru <rafamorenoriquelme2004@gmail.com>
2026-09-23 11:18:40 +0200

    refractor: JQuery to Js Vanilla

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,7 +1,5 @@
-$(document).ready(function () {
-  $(".secret").click(
-    function(){
-        alert("Es un secreto... 🤫")
-    }
-  )
+const secret = document.querySelector('.secret');
+
+secret.addEventListener("click", () => {
+  alert("Es un secreto... 🤫");
 });
